c225cda53cb94e4bc00fc117534372971dc07734
chandansgowda <chandansuresh007@gmail.com>
2023-03-28 19:48:27 +0530

    created room class

---
+++ lib/models/room.dart
@@ -0,0 +1,20 @@
+class Room {
+  Room({
+    required this.name,
+    required this.description,
+  });
+  late final String name;
+  late final String description;
+
+  Room.fromJson(Map<String, dynamic> json){
+    name = json['name'];
+    description = json['description'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['name'] = name;
+    _data['description'] = description;
+    return _data;
+  }
+}
